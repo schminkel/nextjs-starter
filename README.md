72bf9ccf84afa845e1a03771640a9aaea965abbd
THORSTEN SCHMINKEL <thorsten.a.schminkel@capgemini.com>
2024-04-09 21:52:19 +0200

    update starter with prisma

--- README.md
+++ README.md
@@ -5,12 +5,47 @@
 - Typescript
 - ESLint
 - Prettier
+- Prisma with Postgres
+- Shadcn/UI
 
 ## Update all packages
 
 ```bash
 npm install -g npm-check-updates
 ncu --upgrade
+```
+
+## Prisma
+
+```bash
+# create a new prisma project initially
+npx prisma generate
+
+# create a new migration
+npx prisma db push
+
+# open prisma studio
+npx prisma studio
+```
+
+## Postgres
+
+```bash
+# spin up a postgres container locally with docker
+docker run --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=postgres -d postgres
+```
+
+```json
+// change the datasoure in `schema.prisma` to use postgres
+datasource db {
+  provider = "postgresql"
+  url      = env("DATABASE_URL")
+}
+```
+
+```properties
+# add .env file with DATABASE_URL
+DATABASE_URL="postgresql://postgres:postgres@localhost:5432/postgres"
 ```
 
 ## TODOs
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import React from "react";
+import { db } from "@/lib/db";
+import { Task } from "@/types/task";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
-export default function Home(): React.JSX.Element {
+export default async function Home(): Promise<React.JSX.Element> {
+  const tasks: Task[] = await db.task.findMany();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-600 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-50">
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="before:bg-gradient-radial after:bg-gradient-conic relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-600 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-50 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           alt="Next.js Logo"
           className="relative dark:drop-shadow-[0_0_0.4rem_#ffffff90] dark:invert"
@@ -13,6 +24,25 @@ export default function Home(): React.JSX.Element {
           width={180}
           priority
         />
+      </div>
+
+      <div className="pt-16">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-96 font-bold">Id</TableHead>
+              <TableHead className="w-96 font-bold">Title</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tasks.map((task: Task) => (
+              <TableRow key={task.id}>
+                <TableCell>{task.id}</TableCell>
+                <TableCell>{task.title}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </main>
   );
